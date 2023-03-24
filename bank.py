ae866ce92607ae3a962b3eeca0050c320ce71d75
Parveenkaur1027 <127684703+Parveenkaur1027@users.noreply.github.com>
2023-03-24 13:04:43 +0500

    Update bank.py
    Polymorphism, Encapsulation, Inheritance added

--- bank.py
+++ bank.py
@@ -1,109 +1,98 @@
-class BankAccount:
-    def __init__(self, account_number, account_holder_name, account_balance):
-        self.__account_number = account_number
-        self.__account_holder_name = account_holder_name
-        self.__account_balance = account_balance
+'''
+In this updated implementation, we have used Encapsulation to hide the account balance and name attributes 
+of the Account class by making them private (__balance and __name). We also used the get_id(), get_name(),
+ and get_balance() methods to provide public access to these private attributes.
 
-    def get_account_number(self):
-        return self.__account_number
+We have also demonstrated Polymorphism by creating a new SavingsAccount class that inherits from the Account class
+ and has a different interest rate attribute.
 
-    def set_account_number(self, account_number):
-        self.__account_number = account_number
+Finally, we have used Inheritance by making SavingsAccount a subclass of Account. This allows us to reuse the 
+code from the Account class and add new functionality specific to the SavingsAccount class.
+'''
 
-    def get_account_holder_name(self):
-        return self.__account_holder_name
+class Account:
+    def __init__(self, id, name, balance):
+        self.__id = id
+        self.__name = name
+        self.__balance = balance
 
-    def set_account_holder_name(self, account_holder_name):
-        self.__account_holder_name = account_holder_name
+    def get_id(self):
+        return self.__id
 
-    def get_account_balance(self):
-        return self.__account_balance
+    def get_name(self):
+        return self.__name
 
-    def set_account_balance(self, account_balance):
-        self.__account_balance = account_balance
+    def get_balance(self):
+        return self.__balance
 
-    def deposit(self, amount):
-        self.__account_balance += amount
+    def __str__(self):
+        return f"Account ID: {self.__id}\nName: {self.__name}\nBalance: {self.__balance:.2f}"
 
-    def withdraw(self, amount):
-        if amount > self.__account_balance:
-            print("Insufficient balance")
-        else:
-            self.__account_balance -= amount
-
-    def transfer(self, amount, destination_account):
-        if amount > self.__account_balance:
-            print("Insufficient balance")
-        else:
-            self.__account_balance -= amount
-            destination_account.deposit(amount)
-
-
-class SavingsAccount(BankAccount):
-    def __init__(self, account_number, account_holder_name, account_balance, interest_rate, minimum_balance):
-        super().__init__(account_number, account_holder_name, account_balance)
-        self.__interest_rate = interest_rate
-        self.__minimum_balance = minimum_balance
+class SavingsAccount(Account):
+    def __init__(self, id, name, balance):
+        super().__init__(id, name, balance)
+        self.__interest_rate = 0.01
 
     def get_interest_rate(self):
         return self.__interest_rate
 
-    def set_interest_rate(self, interest_rate):
-        self.__interest_rate = interest_rate
+class Bank:
+    def __init__(self):
+        self.__accounts = []
+        self.__next_id = 1
 
-    def get_minimum_balance(self):
-        return self.__minimum_balance
+    def create_account(self, name, initial_deposit):
+        account = Account(self.__next_id, name, initial_deposit)
+        self.__accounts.append(account)
+        self.__next_id += 1
+        return account
 
-    def set_minimum_balance(self, minimum_balance):
-        self.__minimum_balance = minimum_balance
+    def create_savings_account(self, name, initial_deposit):
+        account = SavingsAccount(self.__next_id, name, initial_deposit)
+        self.__accounts.append(account)
+        self.__next_id += 1
+        return account
 
-    def add_interest(self):
-        interest = self.__account_balance * self.__interest_rate / 100
-        self.__account_balance += interest
+    def get_account(self, id):
+        for account in self.__accounts:
+            if account.get_id() == id:
+                return account
+        return None
 
-
-def create_account():
-    account_number = input("Enter account number: ")
-    account_holder_name = input("Enter account holder name: ")
-    account_balance = float(input("Enter account balance: "))
-    account_type = input("Enter account type (1 for savings, 2 for current): ")
-    if account_type == "1":
-        interest_rate = float(input("Enter interest rate: "))
-        minimum_balance = float(input("Enter minimum balance: "))
-        account = SavingsAccount(account_number, account_holder_name, account_balance, interest_rate, minimum_balance)
-    else:
-        account = BankAccount(account_number, account_holder_name, account_balance)
-
-    accounts.append(account)
-    print("Account created successfully")
-
-
-def display_account_details(account):
-    print("Account Number: ", account.get_account_number())
-    print("Account Holder Name: ", account.get_account_holder_name())
-    print("Account Balance: ", account.get_account_balance())
-
-
-# Main Menu
-accounts = []
-
-while True:
-    print("\n\n1. Create Account")
-    print("2. Deposit")
-    print("3. Withdraw")
-    print("4. Transfer")
-    print("5. View Account Details")
-    print("6. Exit")
-    choice = input("Enter your choice: ")
-
-    if choice == "1":
-        create_account()
-    elif choice == "2":
-        account_number = input("Enter account number: ")
-        account = next((acc for acc in accounts if acc.get_account_number() == account_number), None)
+    def deposit(self, id, amount):
+        account = self.get_account(id)
         if account:
-            amount = float(input("Enter deposit amount: "))
-            account.deposit(amount)
-            print("Deposit successful")
+            balance = account.get_balance()
+            balance += amount
+            account._Account__balance = balance
         else:
-            print("Deposit failed")
+            print("Account not found")
+
+    def withdraw(self, id, amount):
+        account = self.get_account(id)
+        if account:
+            balance = account.get_balance()
+            if balance >= amount:
+                balance -= amount
+                account._Account__balance = balance
+            else:
+                print("Insufficient balance")
+        else:
+            print("Account not found")
+
+    def transfer(self, sender_id, recipient_id, amount):
+        sender = self.get_account(sender_id)
+        recipient = self.get_account(recipient_id)
+        if sender and recipient:
+            sender_balance = sender.get_balance()
+            if sender_balance >= amount:
+                sender_balance -= amount
+                sender._Account__balance = sender_balance
+                recipient_balance = recipient.get_balance()
+                recipient_balance += amount
+                recipient._Account__balance = recipient_balance
+                print("Transfer succeccful")
+            else:
+                print("Insufficient balance")
+        else:
+            print("Account not found")
